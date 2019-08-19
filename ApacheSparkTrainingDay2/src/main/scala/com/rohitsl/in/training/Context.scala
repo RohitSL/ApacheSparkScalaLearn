dce421dbac133dea7a5ccdf022abca5a019d9a72
RohitSL <rohitsabharwal@hotmail.com>
2019-08-19 11:42:23 +0530

    Hive support for day2

--- ApacheSparkTrainingDay2/src/main/scala/com/rohitsl/in/training/Context.scala
+++ ApacheSparkTrainingDay2/src/main/scala/com/rohitsl/in/training/Context.scala
@@ -1,21 +1,22 @@
 package com.rohitsl.in.training
-
+import java.io.File
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 trait Context {
-
+  val warehouseLocation = new File("spark-warehouse").getAbsolutePath
   lazy val sparkConf = new SparkConf()
-    .setAppName("My Test Application")
     //.setMaster("spark://localhost:7077")
-    //.config("spark.sql.warehouse.dir", "warehouseLocation")
-    //.enableHiveSupport()
-    .setMaster("local[1]")
-    .set("spark.cores.max", "1")
+    .set("spark.sql.warehouse.dir", warehouseLocation)
+    .setMaster("local[*]")
+    //.set("spark.cores.max", "1")
+    .set("spark.driver.bindAddress", "127.0.0.1")
 
   lazy val sparkSession = SparkSession
     .builder()
     .config(sparkConf)
+    .appName("mySparkApp")
+    .enableHiveSupport()
     .getOrCreate()
   sparkSession.sparkContext.setLogLevel("ERROR")
 }
